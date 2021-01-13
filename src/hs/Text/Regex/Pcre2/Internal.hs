6b42d05c727e7015e888c436a172b473d0c02ad5
amesgen <amesgen@amesgen.de>
2021-01-13 02:25:34 +0100

    future-proof for simplified subsumption in GHC 9.0
    https://gitlab.haskell.org/ghc/ghc/-/wikis/migration/9.0#simplified-subsumption

--- src/hs/Text/Regex/Pcre2/Internal.hs
+++ src/hs/Text/Regex/Pcre2/Internal.hs
@@ -1140,8 +1140,8 @@ getAllSliceRanges matchDataPtr = do
 
 -- | Helper to create non-Template Haskell API functions.  They all take options
 -- and a pattern, and then do something via a 'Matcher'.
-withMatcher :: (Matcher -> a) -> Option -> Text -> a
-withMatcher f option patt = f $ unsafePerformIO $ assembleMatcher option patt
+newMatcher :: Option -> Text -> Matcher
+newMatcher option patt = unsafePerformIO $ assembleMatcher option patt
 
 -- | Match a pattern to a subject once and return a list of captures, or @[]@ if
 -- no match.
@@ -1189,10 +1189,12 @@ matches = matchesOpt mempty
 
 -- | @matchesOpt mempty = matches@
 matchesOpt :: Option -> Text -> Text -> Bool
-matchesOpt = withMatcher $ \matcher -> has $ _capturesInternal
+matchesOpt opt t = has $ _capturesInternal
     matcher
     (const $ return noTouchy)
     noTouchy
+  where
+    matcher = newMatcher opt t
 
 -- | Match a pattern to a subject once and return the portion that matched in an
 -- `Alternative`, or `empty` if no match.
@@ -1286,8 +1288,9 @@ _captures = _capturesOpt mempty
 
 -- | @_capturesOpt mempty = _captures@
 _capturesOpt :: Option -> Text -> Traversal' Text (NonEmpty Text)
-_capturesOpt = withMatcher $ \matcher ->
-    _capturesInternal matcher getAllSliceRanges slice
+_capturesOpt opt t = _capturesInternal matcher getAllSliceRanges slice
+  where
+    matcher = newMatcher opt t
 
 -- | Given a pattern, produce a traversal (0 or more targets) that focuses from
 -- a subject to the portions of it that match.
@@ -1298,8 +1301,9 @@ _match = _matchOpt mempty
 
 -- | @_matchOpt mempty = _match@
 _matchOpt :: Option -> Text -> Traversal' Text Text
-_matchOpt = withMatcher $ \matcher ->
-    _capturesInternal matcher get0thSliceRanges slice . _headNE
+_matchOpt opt t = _capturesInternal matcher get0thSliceRanges slice . _headNE
+  where
+    matcher = newMatcher opt t
 
 -- * Support for Template Haskell compile-time regex analysis
 
